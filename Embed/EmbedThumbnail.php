a5bc411609b723fb03111461cef97434beb1eb1d
nyti <42125546+nyti@users.noreply.github.com>
2018-08-06 03:11:23 +0100

    Initial commit

---
+++ Embed/EmbedThumbnail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Discord\Embed;
+
+/**
+ * Class EmbedThumbnail
+ *
+ * @author  Scrummer <scrummer@gmx.ch>
+ * @package DiscordWebhooks\Embed
+ */
+class EmbedThumbnail extends EmbedImage
+{
+}
